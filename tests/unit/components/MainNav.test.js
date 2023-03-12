ce237caa45c5d0d5575c8678289589243afd35ce
Abhigyan Banerjee <abhigyanbanerjee@gmail.com>
2023-03-12 10:52:19 +0100

    Initial Commit

---
+++ tests/unit/components/MainNav.test.js
@@ -0,0 +1,26 @@
+import { render, screen } from "@testing-library/vue";
+import MainNav from "@/components/MainNav.vue";
+
+describe("MainNav", () => {
+  it("Displays company name", () => {
+    render(MainNav);
+    const companyName = screen.getByText("Bobo Careers");
+    expect(companyName).toBeInTheDocument();
+  });
+
+  it("Displays menu items for navigation", () => {
+    render(MainNav);
+    const navigationMenuItems = screen.getAllByRole("listitem");
+    const navigationMenuTexts = navigationMenuItems.map(
+      (item) => item.textContent
+    );
+    expect(navigationMenuTexts).toEqual([
+      "Teams",
+      "Locations",
+      "Life at Bobo Corp",
+      "How we hire",
+      "Students",
+      "Jobs",
+    ]);
+  });
+});
